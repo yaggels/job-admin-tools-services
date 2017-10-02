1d9d12125760c1085b599e264a50e4524314ce45
jchow2 <jchow2@DCjchow268224.sapient.com>
2017-10-02 14:44:32 -0400

    feat: Change to JDBC data store.
    * Change to JDBC data store using Postgres SQL.
    * Add datasource definition on Spring Boot startup.
    * Include table scripts in resources folder.

--- src/main/java/com/sapient/jat/application/Application.java
+++ src/main/java/com/sapient/jat/application/Application.java
@@ -1,8 +1,18 @@
 package com.sapient.jat.application;
 
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.ContextResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jndi.JndiObjectFactoryBean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -30,5 +40,43 @@ public class Application {
         };
     }
     */
+    
+    @Bean
+	public TomcatEmbeddedServletContainerFactory tomcatFactory() {
+		return new TomcatEmbeddedServletContainerFactory() {
+
+			@Override
+			protected TomcatEmbeddedServletContainer getTomcatEmbeddedServletContainer(
+					Tomcat tomcat) {
+				tomcat.enableNaming();
+				return super.getTomcatEmbeddedServletContainer(tomcat);
+			}
+
+			@Override
+			protected void postProcessContext(Context context) {
+				ContextResource resource = new ContextResource();
+				resource.setName("jdbc/myQuartzDS");
+				resource.setType(DataSource.class.getName());
+				resource.setProperty("driverClassName", "org.postgresql.Driver");
+				resource.setProperty("url", "jdbc:postgresql://localhost:5432/postgres");
+				resource.setProperty("factory", "org.apache.tomcat.jdbc.pool.DataSourceFactory");
+				resource.setProperty("username", "postgres");
+				resource.setProperty("password", "Princess0917");
+
+				context.getNamingResources().addResource(resource);
+			}
+		};
+	}
+
+	@Bean(destroyMethod="")
+	public DataSource jndiDataSource() throws IllegalArgumentException, NamingException {
+		JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
+		bean.setJndiName("java:comp/env/jdbc/myQuartzDS");
+		bean.setProxyInterface(DataSource.class);
+		//bean.setLookupOnStartup(false);
+		bean.afterPropertiesSet();
+		return (DataSource)bean.getObject();
+	}
+	
 
 }
